04eff44f2805abf320120586e88f48fa62a19bd8
yuji96 <y.yama683@gmail.com>
2021-03-24 19:13:51 +0900

    add test file

---
+++ quiz.py
@@ -0,0 +1,13 @@
+    # Hello, World!
+import math, statistics
+
+
+def hoge( a, b= 1):
+    tmp = 1
+    return sum(a)
+
+if __name__ == "__main__":
+    huga={'key1' : 1 , 'key2': 2} #頑張れ
+    hoge(huga.values( ))
+    O = 0 + 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10 + 11 + 12 + 13 + 14 + 15 + 16 + 17 + 18 + 19 + 20 + 21 + 22 + 23 + 24 + 25 + 26 + 27 + 28 + 29
+    print(O)
